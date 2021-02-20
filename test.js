e9559d006a2b0ff74c6939c266b77d8d0cfd8179
EC2 Default User <ec2-user@ip-172-31-47-254.us-east-2.compute.internal>
2021-02-20 02:21:14 +0000

    secondcommit

---
+++ test.js
@@ -0,0 +1,19 @@
+const myWeight = 58;
+
+if(myWeight>=80){
+
+    console.log('もっと頑張りましょう');
+
+}else if(myWeight>=70){
+
+    console.log('確実に成果が出ていますよ');
+
+}else if(myWeight>60){
+    
+    console.log('ついにここまで来ましたね');
+
+}else if(myWeight<=60){
+
+    console.log('ダイエット成功です');
+
+}
